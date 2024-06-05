ad73004c08b751c78dd479d5c1f06b9f6031b8b2
nguyenngocdue <nguyenngocdue>
2024-06-05 21:08:30 +0700

    add new script

--- DesignScript.py
+++ DesignScript.py
@@ -1,38 +1,16 @@
 import clr
-import System
-import math 
-from System.Collections.Generic import *
-
-clr.AddReference("ProtoGeometry")
-from Autodesk.DesignScript.Geometry import *
-
 clr.AddReference('RevitAPI')
-import Autodesk
-from Autodesk.Revit.DB import *
-
 clr.AddReference('RevitAPIUI')
-from Autodesk.Revit.UI import*
-from  Autodesk.Revit.UI.Selection import*
-
-clr.AddReference("RevitNodes")
-import Revit
-clr.ImportExtensions(Revit.Elements)
-clr.ImportExtensions(Revit.GeometryConversion)
+from Autodesk.Revit.UI import *
+from Autodesk.Revit.DB import *
+from Autodesk.Revit.UI.Selection import *
 
 clr.AddReference("RevitServices")
-import RevitServices
 from RevitServices.Persistence import DocumentManager
-from RevitServices.Transactions import TransactionManager
-
-import clr
-clr.AddReference('DSCoreNodes')
-from DSCore import *
-
-
 
 doc = DocumentManager.Instance.CurrentDBDocument
-view = doc.ActiveView
-uidoc = DocumentManager.Instance.CurrentUIApplication.ActiveUIDocument
+uiDoc = DocumentManager.Instance.CurrentUIApplication.ActiveUIDocument
+
 class SelectionFilter(ISelectionFilter):
 	def __init__(self, ctgName1 , ctgName2):
 		self.ctgName1 = ctgName1
@@ -46,14 +24,15 @@ class SelectionFilter(ISelectionFilter):
 	def AllowReference(ref, xyZ):
 		return False
 
+selectionFilter = SelectionFilter("Walls", "Structural Columns")
+selectedElements = uiDoc.Selection.PickElementsByRectangle(selectionFilter, "Select elements")
 
-ele = SelectionFilter("Grids", "Structural Columns")
-elSelectAll = uidoc.Selection.PickElementsByRectangle(ele,"Selects")
-OUT = elSelectAll
+columns = []
+walls = []
+for element in selectedElements:
+    if element.Category.Name == "Structural Columns":
+        columns.append(element)
+    elif element.Category.Name == "Walls":
+        walls.append(element)
 
-grids = []
-column = []
-for i in elSelectAll:
-    if i.Category.Name == "Structural Columns": column.append(i)
-    if i.Category.Name == "Grids": grids.append(i)
-OUT =  column, grids
+OUT = (columns, walls)
---
+++ PythonTemplate.py
@@ -0,0 +1,46 @@
+"""Copyright(c) 2019 by: duengocnguyen@gmail.com"""
+'https://www.youtube.com/channel/UCt2JhCDDFxpYho575WTMZ4g'
+"""________________Welcome to BIM3DM-DYNAMO API___________________"""
+import clr 
+import sys
+sys.path.append(r'C:\Program Files\Autodesk\Revit 2020\AddIns\DynamoForRevit\IronPython.StdLib.2.7.8')
+import math 
+from System.Collections.Generic import *
+
+clr.AddReference("ProtoGeometry")
+from Autodesk.DesignScript.Geometry import *
+
+clr.AddReference('RevitAPI')
+import Autodesk
+from Autodesk.Revit.DB import *
+
+clr.AddReference('RevitAPIUI')
+from Autodesk.Revit.UI import*
+from  Autodesk.Revit.UI.Selection import*
+
+clr.AddReference("RevitNodes")
+import Revit
+clr.ImportExtensions(Revit.Elements)
+clr.ImportExtensions(Revit.GeometryConversion)
+
+clr.AddReference("RevitServices")
+import RevitServices
+from RevitServices.Persistence import DocumentManager
+from RevitServices.Transactions import TransactionManager
+
+doc = DocumentManager.Instance.CurrentDBDocument
+view = doc.ActiveView
+uidoc = DocumentManager.Instance.CurrentUIApplication.ActiveUIDocument
+
+#Preparing input from dynamo to revit
+element = UnwrapElement(IN[0])
+
+#Do some action in a Transaction
+#TransactionManager.Instance.EnsureInTransaction(doc)
+"""Now, You Can Code """
+
+
+
+#TransactionManager.Instance.TransactionTaskDone()
+
+OUT = element
